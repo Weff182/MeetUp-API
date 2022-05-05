bdfae661fd74266ab5f8db30ee64982d79a3f645
weff182 <weffomax@gmail.com>
2022-05-05 07:13:12 +0300

    feat: create DB configuration

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 require('dotenv').config()
 const express = require('express') 
+const sequelize = require('./db')
 const PORT = process.env.PORT
 const app = express()
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
+
