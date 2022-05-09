ec5d41122096fbc5fab036efccbbd31fe50130a0
weff182 <weffomax@gmail.com>
2022-05-09 13:49:44 +0300

    feat: add user model

--- models/models.js
+++ models/models.js
@@ -3,11 +3,22 @@ const {DataTypes} = require('sequelize')
 
 const MeetUp = sequelize.define('meetUp', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    title: {type: DataTypes.STRING, allowNull: false},
+    title: {type: DataTypes.STRING, allowNull: false, unique: true},
     description: {type: DataTypes.STRING, allowNull: false},
     keywords: {type: DataTypes.STRING}, 
     eventInformation: {type: DataTypes.STRING, allowNull: false}
 })
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true, allowNull: false},
+    password: {type: DataTypes.STRING, allowNull: false},
+    role: {type: DataTypes.STRING, defaultValue: 'USER'}
+})
+
+User.hasMany(MeetUp)
+MeetUp.belongsTo(User)
+
 module.exports = {
-    MeetUp
+    MeetUp,
+    User
 }
